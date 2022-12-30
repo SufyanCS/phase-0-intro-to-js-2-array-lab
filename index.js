a0b4d8274ee5a15bb1435b54e22b165e7d8550d9
Sufyan Hussein <sufianb.contact@gmail.com>
2022-12-30 16:54:48 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+console.log(cats)
+function destructivelyRemoveLastCat(name){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+
+    const CopyOfCats = [...cats]
+    CopyOfCats.push(name);
+    return CopyOfCats;
+
+}
+function prependCat(name){
+
+    const CopyOfCats = [...cats]
+    CopyOfCats.unshift(name);
+    return CopyOfCats;
+
+}
+function removeLastCat(){
+
+    const CopyOfCats = [...cats]
+    CopyOfCats.pop();
+    return CopyOfCats;
+
+}
+function removeFirstCat(){
+
+    const CopyOfCats = [...cats]
+    CopyOfCats.shift();
+    return CopyOfCats;
+
+}
